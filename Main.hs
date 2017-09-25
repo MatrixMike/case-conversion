4481db97b11c99e4f0e276d5edc036737b849115
Mike J H <matrixmike@icloud.com>
2017-09-25 12:46:48 +1000

    mini test prog

--- Main.hs
+++ Main.hs
@@ -1,6 +1,8 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 module Main where
 import Text.CaseConversion
 
-main = do
-       interact (unlines . map  (toCase Snake . fromCase Camel)  . lines ) 
+main :: IO()
+main = interact (unlines . map  (toCase Snake . fromCase Camel)  . lines ) 
 
