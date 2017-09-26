7fb889f9db5dd88db8fbc1ffda64bd83a98b345b
Mike J Hewitt <mikeh@electroteach.com>
2017-09-26 20:27:58 +1000

    use this to test library

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
 
 module Main where
+
 import Text.CaseConversion
 
-main :: IO()
+main :: IO ()
 main = do
   putStrLn "starting -> please supply input..."
-  interact (unlines . map  (toCase Snake . fromCase Camel)  . lines ) 
+  interact (unlines . map (toCase Snake . fromCase Camel) . lines)
   putStrLn "finishing..."
