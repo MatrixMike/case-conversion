5079683e40381cf5d2bb6aab1f4e30d20919b04e
MikeJ Hewitt <matroxmike@icloud.com>
2018-01-11 10:55:14 +1100

    better UI comment

--- Main.hs
+++ Main.hs
@@ -6,6 +6,6 @@ import Text.CaseConversion
 
 main :: IO ()
 main = do
-  putStrLn "starting -> please supply input..."
+  putStrLn "starting -> please supply input... in CamelCase"
   interact (unlines . map (toCase Snake . fromCase Camel) . lines)
   putStrLn "finishing..."
